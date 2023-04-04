837490e2de2adbb27f06804aaa7162b53e159970
Suk Wah Ho <sukwah.ho@triosstudent.com>
2023-04-04 09:16:10 -0400

    v3

--- day 5 test1/Suk Wah Ho 22083285 part1.py
+++ day 5 test1/Suk Wah Ho 22083285 part1.py
@@ -2,15 +2,6 @@
 month=int(input("Enter the month in the numeric form : "))
 day=int(input("Enter the day of the month:  "))
 year=int(input("Enter the year in two digit format: "))
-if (month != 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12):
-  print("Error: Invalid Month Input")
-elif (day!= 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31):
-  print("Error: Invalid Day Input")
-elif (year<9 and >100):
-  print(Error: Invalid Year Input")#part1
-month=int(input("Enter the month in the numeric form : "))
-day=int(input("Enter the day of the month:  "))
-year=int(input("Enter the year in two digit format: "))
 if (month <=0) and (month >=31):
   print("Error: Invalid Month Input")
 elif (day <=0) and (day >=13):
@@ -18,5 +9,5 @@ elif (day <=0) and (day >=13):
 elif (year<=9) and  (year>= 100):
   print("Error: Invalid Year Input")
 else:
-  print("The date is " + month,day,yearsep="/") 
+  print("The date is " + str(month),str(day),str(year),sep="/") 
   print("Success: Congratulations! you entered the correct date.")
