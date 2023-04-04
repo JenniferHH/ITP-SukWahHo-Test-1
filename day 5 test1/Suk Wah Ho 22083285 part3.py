87d7b99a2a21451674a49c44ed84088ed8d1f484
Suk Wah Ho <sukwah.ho@triosstudent.com>
2023-04-04 11:26:46 -0400

    v11

--- day 5 test1/Suk Wah Ho 22083285 part3.py
+++ day 5 test1/Suk Wah Ho 22083285 part3.py
@@ -5,27 +5,27 @@ def convertSalary(country, salary):
   if country=="Canada":
     final=salary*56
     if final >64000:
-      print("You will be rich in Canada with a salary of " + final + "CAD")
+      print("You will be rich in Canada with a salary of " ,final, "CAD")
     else:
-      print("You will be poor in Canada with a salary of " + final + "CAD")
+      print("You will be poor in Canada with a salary of " ,final, "CAD")
   elif country=="USA":
     final==salary*1.18
     if final >56516:
-       print("You will be rich in USA with a salary of " + final + " US Dollars")
+       print("You will be rich in USA with a salary of " ,final, " US Dollars")
     else:
-      print("You will be poor in USA with a salary of " + final + " US Dollars")
+      print("You will be poor in USA with a salary of " ,final, " US Dollars")
   elif country=="Cambodia":
     final=salary*4847.38
     if final > 5649856:
-       print("You will be rich in Cambodia with a salary of " + final + " Cambodian Riel")
+       print("You will be rich in Cambodia with a salary of " ,final, " Cambodian Riel")
     else:
-      print("You will be poor in Cambodia with a salary of " + final + " Cambodian Riel")
+      print("You will be poor in Cambodia with a salary of " ,final, " Cambodian Riel")
   elif country=="United Kingdom":
     final=salary*0.91
     if final>35423:
-      print("You will be rich in United Kingdom with a salary of " + final + " Pound Sterling") 
+      print("You will be rich in United Kingdom with a salary of " ,final, " Pound Sterling") 
     else:
-      print("You will be poor in United Kingdom with a salary of " + final + " Pound Sterling")
+      print("You will be poor in United Kingdom with a salary of " ,final, " Pound Sterling")
   else:
     print("Error") 
 
