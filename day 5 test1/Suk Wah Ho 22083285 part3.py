76317068f53400b1a85542efca5665e4c12833cc
Suk Wah Ho <sukwah.ho@triosstudent.com>
2023-04-04 09:57:02 -0400

    v7

--- day 5 test1/Suk Wah Ho 22083285 part3.py
+++ day 5 test1/Suk Wah Ho 22083285 part3.py
@@ -2,4 +2,22 @@
 salary=int(input("Please enter your salary in Germany: "))
 country=input("Enter the country you want to migrate: ")
 def convertSalary():
+  if country=="Canada":
+    finalsalary=int(salary*56)
+  elif country=="USA":
+    finalsalary=int(salary*1.18)
+  elif country=="Cambodia":
+    finalsalary=int(salary*4,847.38)
+  else:
+    finalsalary=int(salary*	
+0.91)
+  convertSalary()
+  if finalsalary>=64000:
+     print("You will be rich in Canada with a salary of " + finalsalary + "CAD")
+    
+
+    
+    
+    
+    
   
