7be576f4a3f43606cca82a7428326313971a45af
Suk Wah Ho <sukwah.ho@triosstudent.com>
2023-04-04 12:07:12 -0400

    v13

--- day 5 test1/Suk Wah Ho 22083285 part3.py
+++ day 5 test1/Suk Wah Ho 22083285 part3.py
@@ -2,25 +2,25 @@
 salary=float(input("Please enter your salary in Germany: "))
 country=input("Enter the country you want to migrate: ")
 def convertSalary(country, salary):
-  if country=="Canada":
+  if country.lower()=="canada":
     final=salary*1.55
     if final >64000:
       print("You will be rich in Canada with a salary of " ,final, "CAD")
     else:
       print("You will be poor in Canada with a salary of " ,final, "CAD")
-  elif country=="USA":
+  elif country.lower()=="usa":
     final=salary*1.18
     if final >56516:
        print("You will be rich in USA with a salary of " ,final, " US Dollars")
     else:
       print("You will be poor in USA with a salary of " ,final, " US Dollars")
-  elif country=="Cambodia":
+  elif country.lower()=="cambodia":
     final=salary*4847.38
     if final > 5649856:
        print("You will be rich in Cambodia with a salary of " ,final, " Cambodian Riel")
     else:
       print("You will be poor in Cambodia with a salary of " ,final, " Cambodian Riel")
-  elif country=="United Kingdom":
+  elif country.lower()=="united kingdom":
     final=salary*1.2
     if final>35423:
       print("You will be rich in United Kingdom with a salary of " ,final, " Pound Sterling") 
