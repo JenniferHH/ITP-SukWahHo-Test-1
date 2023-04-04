c815a36a6f48fa085f65198399a9d301d4bfb88b
Suk Wah Ho <sukwah.ho@triosstudent.com>
2023-04-04 08:41:32 -0400

    v1

---
+++ day 5 test1/Suk Wah Ho 22083285 part1.py
@@ -0,0 +1,7 @@
+#part1
+month=int(input("Enter the month in the numeric form : "))
+print(month)
+day=int(input("Enter the day of the month:  "))
+print(day)
+year=int(input("Enter the year in two digit format: "))
+print(year)
