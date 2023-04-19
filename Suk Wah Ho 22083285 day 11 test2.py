265ad76e308413d4babd8317297a58e60f77a727
Suk Wah Ho <sukwah.ho@triosstudent.com>
2023-04-19 11:50:31 -0400

    Day 11 test2 v13

--- Suk Wah Ho 22083285 day 11 test2.py
+++ Suk Wah Ho 22083285 day 11 test2.py
@@ -3,64 +3,92 @@ option = 0
 print("WELCOME TO THE GRANN'S PHONE DIRECTORY MENU")
 print("1. Add a record")
 print("2. Search a record")
-print("3. U1pdate a record")
+print("3. Update a record")
 print("4. Delete a record")
 print("5. Quit")
 while option != 5:
   option = int(input("Enter your choice: "))
   if option == 1:
     name = input("Enter name: ")
-    phonenumber = input("Enter your 10-digit phone number: ")
-    phoneDirectory.update({name: phonenumber})
-    print("Record added")
-    print("Menu")
-    print("1. Add a record")
-    print("2. Search a record")
-    print("3. Change a record")
-    print("4. Delete a record")
-    print("5. Quit")
-
+    if all(i.isalpha() or i.isspace() for i in name):
+      phonenumber = int(input("Enter your 10-digit phone number: "))
+      if len(str(phonenumber)) != 10:
+        print("Enter again")
+      else:
+        phoneDirectory.update({name: phonenumber})
+        print("Record added")
+        print("Menu")
+        print("1. Add a record")
+        print("2. Search a record")
+        print("3. Change a record")
+        print("4. Delete a record")
+        print("5. Quit")
+    else:
+      print("Enter again")
+    
   elif option == 2:
     name = input("Enter name to search: ")
-    h = True
-    for i in phoneDirectory.keys():
-      if (i == name):
-        print(i + ":" + phoneDirectory[i])
-        h = False
-      if h:
-        print("Not Found")
-        h = h < 1
-    print("Menu")
-    print("1. Add a record")
-    print("2. Search a record")
-    print("3. Change a record")
-    print("4. Delete a record")
-    print("5. Quit")
-
-  elif option == 3:
-    name = input("Enter name: ")
-    phonenumber = input("Enter new 10-digit phone number: ")
-    phoneDirectory.update({name: phonenumber})
-    print("Record updated")
-    print("Menu")
-    print("1. Add a record")
-    print("2. Search a record")
-    print("3. Change a record")
-    print("4. Delete a record")
-    print("5. Quit")
-
-  elif option == 4:
-    if name in phoneDirectory:
-      phoneDirectory.pop(name)
-      print("Record deleted")
+    if all(z.isalpha() or z.isspace() for z in name):
+      h = True
+      for i in phoneDirectory.keys():
+        if (i == name):
+          print(i, ":" , phoneDirectory[i])
+          h = False
+        if h:
+          print("Not Found")
+          h = h < 1
       print("Menu")
       print("1. Add a record")
       print("2. Search a record")
       print("3. Change a record")
       print("4. Delete a record")
       print("5. Quit")
+    else:
+      print("Enter again")
+
+  elif option == 3:
+    name = str(input("Enter name: "))
+    if all(i.isalpha() or i.isspace() for i in name):
+      phonenumber = int(input("Enter new 10-digit phone number: "))
+      if len(str(phonenumber)) != 10:
+        print("Enter again")
+      else:
+        h = True
+        for i in phoneDirectory.keys():
+          if (i == name):
+            phoneDirectory.update({name: phonenumber})
+            print("Record updated")
+            h = False
+          elif h:
+            print("Not Found")
+            h = h < 1
+        print("Menu")
+        print("1. Add a record")
+        print("2. Search a record")
+        print("3. Change a record")
+        print("4. Delete a record")
+        print("5. Quit")
+    else:
+      print("Enter again")
+
+  elif option == 4:
+    name = str(input("Enter name: "))
+    if all(i.isalpha() or i.isspace() for i in name):
+      if name in phoneDirectory:
+        phoneDirectory.pop(name)
+        print("Record deleted")
+      else:
+       print("Not found")
+       print("Menu")
+       print("1. Add a record")
+       print("2. Search a record")
+       print("3. Change a record")
+       print("4. Delete a record")
+       print("5. Quit")
+    else:
+      print("Enter again")
 
   elif option == 5:
     print("Quit")
   else:
-    print("Wrong Option Entered.")
+    print("Wrong Option Entered")
