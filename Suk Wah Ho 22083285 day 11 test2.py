f5620414775664af4ec1d0fd3a58aebbf284da68
Suk Wah Ho <sukwah.ho@triosstudent.com>
2023-04-19 09:22:25 -0400

    Day 11 test2 v6

--- Suk Wah Ho 22083285 day 11 test2.py
+++ Suk Wah Ho 22083285 day 11 test2.py
@@ -1,48 +1,66 @@
-phoneDirectory={}
-option=0
+phoneDirectory = {}
+option = 0
 print("WELCOME TO THE GRANN'S PHONE DIRECTORY MENU")
 print("1. Add a record")
 print("2. Search a record")
 print("3. Update a record")
 print("4. Delete a record")
 print("5. Quit")
-while option!=5:
-   option=int(input("Enter your choice: "))
-   if option==1:
-    name=input("Enter name: ")
-    phonenumber=input("Enter your 10-digit phone number: ")
-    phoneDirectory.update({name:phonenumber})
+while option != 5:
+  option = int(input("Enter your choice: "))
+  if option == 1:
+    name = input("Enter name: ")
+    phonenumber = input("Enter your 10-digit phone number: ")
+    phoneDirectory.update({name: phonenumber})
     print("Record added")
-     
-   elif option==2:
-    productname=input("Enter name to search: ")
-    h=True
-    for i in phoneDirectory.keys():
-       if (i==name):
-        print(i+":"+phoneDirectory[i])
-        h=False
-       if h:
-        print("Not Found")
-        h=h<1
-         
-   elif option==3:
-       name=input("Enter name: ")
-       phonenumber=input("Enter new 10-digit phone number: ")   
-       phoneDirectory.update({name:phonenumber})
-       print("Record updated")
+    print("Menu")
+    print("1. Add a record")
+    print("2. Search a record")
+    print("3. Change a record")
+    print("4. Delete a record")
+    print("5. Quit")
 
-   elif option==4:
-     if name in phoneDirectory:
+  elif option == 2:
+    productname = input("Enter name to search: ")
+    h = True
+    for i in phoneDirectory.keys():
+      if (i == name):
+        print(i + ":" + phoneDirectory[i])
+        h = False
+      if h:
+        print("Not Found")
+        h = h < 1
+    print("Menu")
+    print("1. Add a record")
+    print("2. Search a record")
+    print("3. Change a record")
+    print("4. Delete a record")
+    print("5. Quit")
+
+  elif option == 3:
+    name = input("Enter name: ")
+    phonenumber = input("Enter new 10-digit phone number: ")
+    phoneDirectory.update({name: phonenumber})
+    print("Record updated")
+    print("Menu")
+    print("1. Add a record")
+    print("2. Search a record")
+    print("3. Change a record")
+    print("4. Delete a record")
+    print("5. Quit")
+
+  elif option == 4:
+    if name in phoneDirectory:
       phoneDirectory.pop(name)
       print("Record deleted")
-   elif option==5:
-    print("Quit")
-   else:
-    print("Wrong Option Entered.")
-    
+      print("Menu")
+      print("1. Add a record")
+      print("2. Search a record")
+      print("3. Change a record")
+      print("4. Delete a record")
+      print("5. Quit")
 
-        
-    
-        
-    
-    
+  elif option == 5:
+    print("Quit")
+  else:
+    print("Wrong Option Entered.")
