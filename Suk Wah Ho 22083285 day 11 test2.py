85860212f4553b138922dc5743e83c9ebb683ae7
Suk Wah Ho <sukwah.ho@triosstudent.com>
2023-04-19 08:57:09 -0400

    Day 11 test2 v2

--- Suk Wah Ho 22083285 day 11 test2.py
+++ Suk Wah Ho 22083285 day 11 test2.py
@@ -6,3 +6,10 @@ print("2. Search a record")
 print("3. Update a record")
 print("4. Delete a record")
 print("5. Quit")
+while option!=5:
+   option=int(input("Enter your choice: "))
+   if option==1:
+    name=input("Enter name: ")
+    phonenumber=input("Enter your 10-digit phone number: ")
+   phoneDirectory.update({name:phonenumber})
+   print("Record added")
