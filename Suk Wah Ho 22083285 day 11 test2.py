f464f85c14ef571fb6ff42e136d3c9f68c81e004
Suk Wah Ho <sukwah.ho@triosstudent.com>
2023-04-19 08:49:21 -0400

    Day 11 test2 v1

---
+++ Suk Wah Ho 22083285 day 11 test2.py
@@ -0,0 +1,8 @@
+phoneDirectory={}
+option=0
+print("WELCOME TO THE GRANN'S PHONE DIRECTORY MENU")
+print("1. Add a record")
+print("2. Search a record")
+print("3. Update a record")
+print("4. Delete a record")
+print("5. Quit")
